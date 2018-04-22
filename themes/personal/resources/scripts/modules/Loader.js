86f58a4875c1b136e832331a8b371013efed49ad
Shelby Stanton <shelby_@hotmail.co.uk>
2018-04-22 14:27:02 +0100

    Revert

--- themes/personal/resources/scripts/modules/Loader.js
+++ themes/personal/resources/scripts/modules/Loader.js
@@ -1,9 +1,13 @@
 export default function Loader() {
 	const body = document.querySelector(`body`);
-	body.classList.add('scroll-control');
 
-    setTimeout(function(){
-    	body.classList.remove('scroll-control');
-        body.classList.add('loaded');
-    }, 2500);
+	$(document).ready(function() {
+		body.classList.add('scroll-control');
+
+	    setTimeout(function(){
+	    	body.classList.remove('scroll-control');
+	        body.classList.add('loaded');
+	    }, 2500);
+	});
+
 }
